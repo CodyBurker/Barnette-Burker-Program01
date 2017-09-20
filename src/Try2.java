1ed14a328a7699e26c5bf36364ad455a0f72d69f
Cody Burker <codyburker@gmail.com>
2017-09-20 13:36:43 -0400

    Completed.

--- src/Try2.java
+++ src/Try2.java
@@ -1,10 +1,6 @@
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Stack;
-
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
@@ -12,121 +8,149 @@ import javax.script.ScriptException;
 public class Try2 {
 
 	public static void main(String[] args) throws NumberFormatException, ScriptException {
-		Stack<Integer> inputList = getInput(); // Get user input
-		// Make an ArrayList of ArrayLists,
-		// each inner ArrayList contains permeations of numbers.
-		Stack<Stack<Integer>> numberPermeations = permute(inputList, 0);
-		Stack<Stack<Character>> operatorCombinations = generatorOperatorStack();
-		Stack<Stack<String>> parenthesesCombinations = buildParentheseStack();
+		ArrayList<Integer> inputArrayList = getInput(); // Get user input
+		// Make an ArrayArrayList of ArrayArrayLists,
+		// each inner ArrayArrayList contains permeations of numbers.
+		ArrayList<ArrayList<Integer>> numberPermeations = new ArrayList<ArrayList<Integer>>();
+		permute(inputArrayList, 0, numberPermeations);
 
-		// Build a list of strings with all combinations of expressions
-		LinkedList<String> expressions = new LinkedList<>();
-		for (Stack<Integer> numbers : numberPermeations) {
-			for (Stack<Character> character : operatorCombinations) {
-				for (Stack<String> parenthesis : parenthesesCombinations) {
+		ArrayList<ArrayList<Character>> operatorCombinations = generatorOperatorArrayList();
+		ArrayList<String[]> parenthesesCombinations = buildParentheseArrayList();
+		// Build a ArrayList of strings with all combinations of expressions
+		ArrayList<String> expressions = new ArrayList<>();
+		for (ArrayList<Integer> number : numberPermeations) {
+			for (ArrayList<Character> operator : operatorCombinations) {
+				for (String[] parenthesis : parenthesesCombinations) {
 					// Using string builder to avoid concatenation overhead
 					// Pattern is as follows
 					// (1+(2)+(3)+4)
 					StringBuilder exp = new StringBuilder();
-					exp.append(parenthesis.pop());
-					exp.append(numbers.pop());
-					exp.append(character.pop());
-					exp.append(parenthesis.pop());
-					exp.append(numbers.pop());
-					exp.append(parenthesis.pop());
-					exp.append(character.pop());
-					exp.append(parenthesis.pop());
-					exp.append(numbers.pop());
-					exp.append(parenthesis.pop());
-					exp.append(character.pop());
-					exp.append(numbers.pop());
-					exp.append(parenthesis.pop());
-					System.out.println(exp.toString());
-					expressions.add(exp.toString());
 
+					exp.append(parenthesis[0]);
+					exp.append(number.get(0));
+					exp.append(operator.get(0));
+					exp.append(parenthesis[1]);
+					exp.append(number.get(1));
+					exp.append(parenthesis[2]);
+					exp.append(operator.get(1));
+					exp.append(parenthesis[3]);
+					exp.append(number.get(2));
+					exp.append(parenthesis[4]);
+					exp.append(operator.get(2));
+					exp.append(number.get(3));
+					exp.append(parenthesis[5]);
+
+					// expressions eventually has 24*256*7 = 43,008 entries.
+					expressions.add(exp.toString());
 				}
 			}
+		}
+
+		// Evaluate expressions
+
+		System.out.println("Evaluating.");
+		ArrayList<String> validExpressions = evaluate(expressions, 24);
+		System.out.println("Results:");
+		int i = 0;
+		for (String current : validExpressions) {
+			System.out.println(i + ": " + current);
+			i++;
 		}
 
 	}
 
 	// Get 4 integers from user as array
-	static Stack<Integer> getInput() {
+	static ArrayList<Integer> getInput() {
 		System.out.println("Enter four integers");
 		Scanner inputScanner = new Scanner(System.in);
-		Stack<Integer> inputList = new Stack<>();
-		inputList.add(inputScanner.nextInt());
-		inputList.add(inputScanner.nextInt());
-		inputList.add(inputScanner.nextInt());
-		inputList.add(inputScanner.nextInt());
+		ArrayList<Integer> inputArrayList = new ArrayList<>();
+		inputArrayList.add(inputScanner.nextInt());
+		inputArrayList.add(inputScanner.nextInt());
+		inputArrayList.add(inputScanner.nextInt());
+		inputArrayList.add(inputScanner.nextInt());
 		inputScanner.close();
-		return inputList;
+		return inputArrayList;
 	}
 
-	// Method to evaluate string to expression
-	static int evaluate(String expression) throws NumberFormatException, ScriptException {
+	// Method to evaluate ArrayList<String> to expression
+	static ArrayList<String> evaluate(ArrayList<String> expressions, int targetNumber)
+			throws NumberFormatException, ScriptException {
 		ScriptEngineManager manager = new ScriptEngineManager();
 		ScriptEngine javascriptEngine = manager.getEngineByName("javascript");
-		Object returnObject = javascriptEngine.eval(expression);
-		// If return value is an integer return value
-		if (returnObject instanceof Integer) {
-			return (int) returnObject;
-		}
-		// Otherwise, return -1
-		else {
-			return -1;
-		}
-	}
+		ArrayList<String> returnList = new ArrayList<>();
+		for (String expression : expressions) {
+			Object returnObject = javascriptEngine.eval(expression);
+			// If return value is an integer and
+			if (returnObject instanceof Integer) {
+				// if that integer is equal to 24 (the target value) and
 
-	// Method to permute through list
-	// Inspiration drawn from
-	// https://stackoverflow.com/questions/2920315/permutation-of-array
-	static <T> Stack<Stack<T>> permute(Stack<T> list, int bounds) {
-		Stack<Stack<T>> returnList = new Stack<>();
+				if ((int) returnObject == targetNumber) {
+					// Then add it to the list.
+					if (!returnList.contains(expression)) {
+						returnList.add(expression);
+					}
+				}
+			} else if (returnObject instanceof Double) {
+				if ((Double) returnObject == (double) targetNumber) {
 
-		for (int i = bounds; i < list.size(); i++) {
-			Collections.swap(list, i, bounds);
-			// Call self recursively while adding all previous selves.
-			returnList.addAll(permute(list, bounds + 1));
-			Collections.swap(list, bounds, i);
-		}
-		if (bounds == list.size() - 1) {
-			// add current permeation to return list
-			returnList.add(list);
-			// Optionally print to console
-			// System.out.println(Arrays.toString(list.toArray()));
-			// return returnList if at bottom of barrel...
-			return returnList;
+					returnList.add(expression);
+				}
+			}
 		}
 		return returnList;
 	}
 
-	// Method to generate a stack with all possible combination of operators
+	// Method to permute through ArrayList
+	// Inspiration drawn from
+	// https://ArrayListoverflow.com/questions/2920315/permutation-of-array
+	static void permute(ArrayList<Integer> ArrayList, int bounds, ArrayList<ArrayList<Integer>> output) {
+
+		for (int i = bounds; i < ArrayList.size(); i++) {
+			Collections.swap(ArrayList, i, bounds);
+			// Call self recursively while adding all previous selves.
+			permute(ArrayList, bounds + 1, output);
+			Collections.swap(ArrayList, bounds, i);
+		}
+		if (bounds == ArrayList.size() - 1) {
+			// add current permeation to return ArrayList
+			ArrayList<Integer> tmpLst = new ArrayList<>();
+			tmpLst.add(ArrayList.get(0));
+			tmpLst.add(ArrayList.get(1));
+			tmpLst.add(ArrayList.get(2));
+			tmpLst.add(ArrayList.get(3));
+			output.add(tmpLst);
+			// Optionally print to console
+			// return returnArrayList if at bottom of barrel...
+		}
+	}
+
+	// Method to generate a ArrayList with all possible combination of operators
 	// I'm sure there's a more elegant way to accomplish this...
 	// I just don't know what it is.
-	static Stack<Stack<Character>> generatorOperatorStack() {
-		Stack<Stack<Character>> operatorStack = new Stack<>();
+	static ArrayList<ArrayList<Character>> generatorOperatorArrayList() {
+		ArrayList<ArrayList<Character>> operatorArrayList = new ArrayList<>();
 		char[] operators = { '+', '-', '*', '/' };
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
 				for (int k = 0; k < 4; k++) {
 					for (int l = 0; l < 4; l++) {
-						Stack<Character> tempStack = new Stack<>();
-						tempStack.push(operators[i]);
-						tempStack.push(operators[j]);
-						tempStack.push(operators[k]);
-						tempStack.push(operators[l]);
-						operatorStack.push(tempStack);
-						// System.out.println(tempStack.toString());
+						ArrayList<Character> tempArrayList = new ArrayList<>();
+						tempArrayList.add(operators[i]);
+						tempArrayList.add(operators[j]);
+						tempArrayList.add(operators[k]);
+						tempArrayList.add(operators[l]);
+						operatorArrayList.add(tempArrayList);
+						// System.out.println(tempArrayList.toString());
 					}
 				}
 			}
 		}
-		return operatorStack;
+		return operatorArrayList;
 	}
 
-	// Method to build a stack of stacks of strings that represent parentheses
-	public static Stack<Stack<String>> buildParentheseStack() {
+	// Method to build a ArrayList of ArrayLists of strings that represent
+	// parentheses
+	public static ArrayList<String[]> buildParentheseArrayList() {
 		// Long, tedious, ugly code that could be better.
 
 		// PARENTHESE FORMAT IS AS FOLLOWS
@@ -135,7 +159,7 @@ public class Try2 {
 		// 0b00123456
 		// Above is position in byte represented by parentheses
 
-		Stack<Stack<String>> parenPossibilities = new Stack<>();
+		ArrayList<String[]> parenPossibilities = new ArrayList<>();
 		// A + B + C + D
 		String[] variable1 = { "", "", "", "", "", "" };
 		// (A + B)+ C + D
@@ -151,46 +175,13 @@ public class Try2 {
 		// (A + B) + (C + D)
 		String[] variable7 = { "(", "", ")", "(", "", ")" };
 
-		List<String> tempList = Arrays.asList(variable1);
-		Stack<String> tempStack = new Stack<>();
-		tempStack.addAll(tempList);
-		parenPossibilities.push(tempStack);
-
-		List<String> tempList2 = Arrays.asList(variable2);
-		Stack<String> tempStack2 = new Stack<>();
-		tempStack2.clear();
-		tempStack2.addAll(tempList2);
-		parenPossibilities.push(tempStack2);
-
-		List<String> tempList3 = Arrays.asList(variable3);
-		Stack<String> tempStack3 = new Stack<>();
-		tempStack3.clear();
-		tempStack3.addAll(tempList3);
-		parenPossibilities.push(tempStack3);
-
-		List<String> tempList4 = Arrays.asList(variable4);
-		Stack<String> tempStack4 = new Stack<>();
-		tempStack4.clear();
-		tempStack4.addAll(tempList4);
-		parenPossibilities.push(tempStack4);
-
-		List<String> tempList5 = Arrays.asList(variable5);
-		Stack<String> tempStack5 = new Stack<>();
-		tempStack5.clear();
-		tempStack5.addAll(tempList5);
-		parenPossibilities.push(tempStack5);
-
-		List<String> tempList6 = Arrays.asList(variable6);
-		Stack<String> tempStack6 = new Stack<>();
-		tempStack6.clear();
-		tempStack6.addAll(tempList6);
-		parenPossibilities.push(tempStack6);
-
-		List<String> tempList7 = Arrays.asList(variable7);
-		Stack<String> tempStack7 = new Stack<>();
-		tempStack7.clear();
-		tempStack7.addAll(tempList7);
-		parenPossibilities.push(tempStack7);
+		parenPossibilities.add(variable1);
+		parenPossibilities.add(variable2);
+		parenPossibilities.add(variable3);
+		parenPossibilities.add(variable4);
+		parenPossibilities.add(variable5);
+		parenPossibilities.add(variable6);
+		parenPossibilities.add(variable7);
 
 		return parenPossibilities;
 	}
